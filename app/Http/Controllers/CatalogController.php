44dea0eb6fa429299898d53e5b3cbd0ca3cbdc96
RRomani <rromaniaguado@iesesteveterradas.cat>
2018-01-29 17:56:37 +0100

    Cap. 1 y parte del 2 compeltados

---
+++ app/Http/Controllers/CatalogController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CatalogController extends Controller
+{
+
+  function getIndex(){
+    	return view('catalog.index', array('arrayPeliculas'=>Movie::all()));
+    }
+    function getShow($id){
+    	return view('catalog.show', array('pelicula'=>Movie::findOrFail($id)));
+    }
+    function getCreate(){
+    	return view('catalog.create');
+    }
+    function getEdit($id){
+    	return view('catalog.edit', array('pelicula'=>Movie::findOrFail($id)));
+    }
+}
